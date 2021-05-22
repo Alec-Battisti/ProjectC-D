f4b8b1cf4631cdecaa12c0d9a8b67ba768c6a80a
Alec-Battisti <47126858+Alec-Battisti@users.noreply.github.com>
2021-05-21 20:23:14 -0700

    Create Comparator_Multiplier.v

---
+++ Comparator_Multiplier.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+
+
+module comparator(
+    input [5:0] Comp_A,Comp_B,
+    output CompO
+    );
+    assign CompO = (Comp_A == Comp_B) ? 1'b1 : 1'b0;
+endmodule
+
+module mult_ten(
+    input [4:0] in,
+    output [6:0] out
+    );
+    wire [6:0] xIn;
+    assign xIn = {2'b00,in};
+    assign out = (xIn <<< 3) + (xIn <<< 1);
+endmodule
